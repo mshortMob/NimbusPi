35a952984114669f47ad3019556744d82b2ee0ad
mshort <mshort@mobiquityinc.com>
2020-11-21 16:18:45 -0500

    start making image uploader

---
+++ editors/maskImageUploadServer.js
@@ -0,0 +1,60 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const express = require("express");
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+// put the HTML file containing your form in a directory named "public" (relative to where this script is located)
+app.get("/", express.static(path.join(__dirname, "./public")));
+
+const multer = require("multer");
+
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+  dest: "./"
+});
+
+
+app.post(
+  "/upload",
+  upload.single("file"),
+  (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./image.png");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/html")
+          .end("File uploaded! <script>document.location.href='http://localhost/editors/maskEditor.html';</script>");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    }
+  }
+);
