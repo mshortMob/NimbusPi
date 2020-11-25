c01b6942d6c88838d6a9892e397b8d466f7a3a3e
mshort <mshort@mobiquityinc.com>
2020-11-25 16:20:52 -0500

    add crude maskUploader page

--- php/writeBase64.php
+++ php/writeBase64.php
@@ -2,8 +2,9 @@
     //$formInput=$_GET['key'];
     $formInput=file_get_contents('php://input');
     $formInput = str_replace('data:image/png;base64,', '', $formInput);
+    $formInput = str_replace('data:image/jpeg;base64,', '', $formInput);
     $formInput = str_replace(' ', '+', $formInput);
-	$formInput = base64_decode($formInput);
+    $formInput = base64_decode($formInput);
     $fn=$_GET['fn'];
     $fn="../".$fn;
     $myfile = fopen($fn, "w") or die("Unable to open file!");
