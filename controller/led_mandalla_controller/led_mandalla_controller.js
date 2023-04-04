2244b08716d556d2811ebc2b0af4dcd67af27682
Matt Short <mshort@mobiquityinc.com>
2023-04-04 19:27:32 -0400

    add copy paste to LED_Mandala_Controller, NOBUILD

--- controller/led_mandalla_controller/led_mandalla_controller.js
+++ controller/led_mandalla_controller/led_mandalla_controller.js
@@ -22,6 +22,10 @@ function init(){
   patternNumber=56;
   selectedFixtures=[0,1,2,3];
   loopTime=1000;
+  copySceneSource=null;
+  copySceneTarget=null;
+  copyFixtureSource=null;
+  copyFixtureTarget=null;
   loopIsPlaying=false;
   apcMiniPort="None";
   fixtureBrightness=[1,1,1,1];
@@ -103,9 +107,22 @@ apcMiniInput.on('message', (deltaTime, message) => {
       console.log("Saved Preset To Row");
     }
     if(message[1]>=0 && message[1]<64 && (message[1]%8)<=3){
-      patternNumber=message[1];
-      syncArtnetToModel();
-      syncLeds();
+      if(!isShift){
+        patternNumber=message[1];
+        syncArtnetToModel();
+        syncLeds();
+      }else{
+        if(copySceneSource==null){
+          copySceneSource=message[1];
+          syncLeds();
+        }else{
+          copySceneTarget=message[1];
+          copyScene(copySceneSource,copySceneTarget,[0,1,2,3]);
+          copySceneSource=null;
+          copySceneTarget=null;
+          syncLeds();
+        }
+      }
     }
     if(message[1]>=0 && message[1]<64 && (message[1]%8)>3){
       loopCellsState[message[1]]=!loopCellsState[message[1]];
@@ -114,8 +131,21 @@ apcMiniInput.on('message', (deltaTime, message) => {
     }
     //fixture selectors
     if(message[1]>=64 && message[1]<68 && message[2]==127){
-      fixtureButtonPress(message[1]-64);
-      syncLeds();
+      if(!isShift){
+        fixtureButtonPress(message[1]-64);
+        syncLeds();
+      }else{
+        if(copyFixtureSource==null){
+          copyFixtureSource=message[1]-64;
+          syncLeds();
+        }else{
+          copyFixtureTarget=message[1]-64;
+          copyFixture(copyFixtureSource,copyFixtureTarget);
+          copyFixtureSource=null;
+          copyFixtureTarget=null;
+          syncLeds();
+        }
+      }
     }
     //start / stop
     if(message[1]==89 && message[2]==127){
@@ -161,6 +191,11 @@ apcMiniInput.on('message', (deltaTime, message) => {
     }else{
       isShift=false;
     }
+    copySceneSource=null;
+    copySceneTarget=null;
+    copyFixtureSource=null;
+    copyFixtureTarget=null;
+    syncLeds();
     console.log(isShift);
   }
   //apc mini sliders input
@@ -207,7 +242,7 @@ apcMiniInput.on('message', (deltaTime, message) => {
         }
       }
       if(message[1]==56 && !isShift){
-          loopTime=200+message[2]*20;
+          loopTime=200+message[2]*50;
           if(loopIsPlaying){
             loopRoWStop();
             loopRoWStart(loopTime);
@@ -241,6 +276,16 @@ app.get('/test2', (req, res) => {
   res.send("Hello World!");
   syncLeds();
   sendArtnet([175,10,255,255,255,255,255,255,255,255,255]);
+})
+
+app.get('/test3', (req, res) => {
+  copyFixture(0,1);
+  res.send("Hello World!3");
+})
+
+app.get('/test4', (req, res) => {
+  copyScene(56, 57, [0,1,2,3]);
+  res.send("Hello World!4");
 })
 
 app.get('/save', (req, res) => {
@@ -281,11 +326,27 @@ function save(){
 function syncLeds(){
   for(var x=0;x<100;x++){
     if(x<64){
-      if(x==patternNumber){
-        apcMiniOutput.sendMessage([144,x,3]); 
+      if(!isShift){
+        if(x==patternNumber){
+          apcMiniOutput.sendMessage([144,x,3]); 
+        }else{
+          if((x%8)<=3){
+            apcMiniOutput.sendMessage([144,x,127]); 
+          }else{
+            if(loopCellsState[x]){
+              apcMiniOutput.sendMessage([144,x,3]); 
+            }else{
+              apcMiniOutput.sendMessage([144,x,5]); 
+            }
+          }
+        }
       }else{
         if((x%8)<=3){
-          apcMiniOutput.sendMessage([144,x,127]); 
+          if(copySceneSource==x){
+            apcMiniOutput.sendMessage([144,x,127]); 
+          }else{
+            apcMiniOutput.sendMessage([144,x,3]); 
+          }
         }else{
           if(loopCellsState[x]){
             apcMiniOutput.sendMessage([144,x,3]); 
@@ -300,10 +361,18 @@ function syncLeds(){
       }
     }
     if(x>=64 && x<68){
-      if(selectedFixtures.indexOf((x-64))!=-1){
-        apcMiniOutput.sendMessage([144,x,127]); 
+      if(!isShift){
+        if(selectedFixtures.indexOf((x-64))!=-1){
+          apcMiniOutput.sendMessage([144,x,127]); 
+        }else{
+          apcMiniOutput.sendMessage([144,x,0]); 
+        }
       }else{
-        apcMiniOutput.sendMessage([144,x,0]); 
+        if((x-64)==copyFixtureSource){
+          apcMiniOutput.sendMessage([144,x,127]); 
+        }else{
+          apcMiniOutput.sendMessage([144,x,0]); 
+        }
       }
     }
     if(x>=68 && x<72){
@@ -389,4 +458,18 @@ function loopRowOld(freq){
 function loopRoWStop(){
   clearInterval(intervalHandle);
   loopIsPlaying=false;
+}
+
+function copyScene(sourceScene, targetScene, includedFixtures){
+  for(var x=0; x<4; x++){
+    if(includedFixtures.indexOf(x)!=-1){
+      channels[targetScene][x]=JSON.parse(JSON.stringify(channels[sourceScene][x])); 
+    }
+  }
+  syncArtnetToModel();
+}
+
+function copyFixture(sourceFixture, targetFixture){
+  channels[patternNumber][targetFixture]=JSON.parse(JSON.stringify(channels[patternNumber][sourceFixture])); 
+  syncArtnetToModel();
 }
