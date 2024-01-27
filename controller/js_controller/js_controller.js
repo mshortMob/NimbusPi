164ba05fc63782923a6c5ec9589409b42cc55c5a
Matt Short <mshort@mobiquityinc.com>
2024-01-27 18:08:43 -0500

    js laser controller stabilish - adding cruise mode

--- controller/js_controller/js_controller.js
+++ controller/js_controller/js_controller.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import fs from "fs";
 import path from "path";
 import WebSocket, { WebSocketServer } from 'ws';
-
+import {exec} from "child_process";
 const wss = new WebSocketServer({ port: 3003 });
 const app = express();
 const port=3002
@@ -22,144 +22,12 @@ var artnetOptions = {
 const artnet = Artnet(artnetOptions);
 
 const stick = new Joystick("/dev/input/js0");
-console.log("xxxxx");
+console.log("initializing joystick:");
 console.log(stick);
 
-init();
-var laserData, selectedPreset, lastButtonPressed;
-var isControllerFxMode=false;
-var isShift=false;
-var isCruiseMode=false;
-var cruiseIntervalHandle=0;
+stick.on("error", (err) => console.log('error initializing joystick!', err));
 
-var jsPositions = {
-  "left-stick":{
-    "xInterval":0,
-    "yInterval":0
-  },
-  "right-stick":{
-    "xInterval":0,
-    "yInterval":0
-  },
-  "pad":{
-    "xInterval":0,
-    "yInterval":0
-  },
-  "pad-shift":{
-    "xInterval":0,
-    "yInterval":0
-  }
-}
-
-function init(){
-  laserData={};
-  selectedPreset=1;
-  for(var x=1; x<17; x++){
-      laserData["scene"+x]={"color":1, "gobo":1, "positionX":0, "positionY":0, "scaleX":0, "scaleY":0, "rotation":0, "zoom":0, "animation":0, "dots":0  }
-  }
-  // savePresets();
-  recallPresets();
-}
-
-function recallPresets(){
-  fs.readFile('/root/laserControllerImages/presets1.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    laserData=JSON.parse(data);
-    console.log('recalled saved presets!');
-    emitEvent();
-  });
-  sendArtnet();
-}
-
-function savePresets(){
-  var fileContents=JSON.stringify(laserData);
-  fs.writeFile('/root/laserControllerImages/presets1.txt', fileContents, err => {
-    if (err) {
-      console.error(err);
-    }
-    console.log('saved preset data!');
-  });
-}
-
-function sendArtnet(){
-  var dmxValues=getDMXfromLaserData(laserData["scene"+selectedPreset]);
-  // var dmxValues=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-  artnet.set(0,1, dmxValues, function (err, res) {
-    console.log("Sent Artnet");
-    // console.log(dmxValues);
-  });
-}
-
-function getDMXfromLaserData(data){
-  var dmxData=[];
-  var colorMapping=[0,15,18,22,27,32,37,10,50,255];
-  var goboMapping=[3,5,2,29,32,33,75,69,44,43,62,79,85,0,1,7];
-  for(var x=1; x<25; x++){
-      if(x==1 || x==1+12){
-          dmxData.push(255);
-      }else if( x==2 || x==(2+12) ){
-          dmxData.push( colorMapping[data.color-1] );
-      }else if( x==3 || x==(3+12) ){
-          dmxData.push( goboMapping[data.gobo-1] );
-      
-      }else if( x==4 || x==(4+12) ){
-          dmxData.push( data.positionY );
-      }else if( x==5 || x==(5+12) ){
-          dmxData.push( data.positionX );
-      }else if( x==6 || x==(6+12) ){
-          dmxData.push( data.scaleY );
-      }else if( x==7 || x==(7+12) ){
-          dmxData.push( data.scaleX );
-      }else if( x==8 || x==(8+12) ){
-          dmxData.push( data.rotation );
-      }else if( x==9 || x==(9+12) ){
-          dmxData.push( data.zoom );
-      }else if( x==11 || x==(11+12) ){
-          dmxData.push( data.dots );
-      }else if( x==12 || x==(12+12) ){
-          dmxData.push( data.animation );
-      }else{
-          dmxData.push(0);
-      }
-  }
-  return dmxData;
-}
-
-function emitEvent(){
-  var messagePayload={};
-  messagePayload["selectedPreset"]=selectedPreset;
-  messagePayload["laserData"]=laserData;
-  messagePayload["lastButtonPressed"]=lastButtonPressed;
-  messagePayload["isControllerFxMode"]=isControllerFxMode;
-  messagePayload["isCruiseMode"]=isCruiseMode;
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(messagePayload));
-    }
-  });
-  sendArtnet();
-}
-
-function extractJoystickEvents(inputEvent, label, type, number, value, comparator, action){
-  if(comparator==">"){
-    if(inputEvent.type == type && inputEvent.number == number && inputEvent.value > value){
-      console.log(label);
-      lastButtonPressed=label;
-      action();
-      emitEvent();
-    }
-  }else{
-    if(inputEvent.type == type && inputEvent.number == number && inputEvent.value <= value){
-      console.log(label);
-      lastButtonPressed=label;
-      action();
-      emitEvent();
-    }   
-  }
-}
+stick.on("disconnect", () => console.log('joystick disconnected'));
 
 stick.on("update", (ev) => {
 
@@ -300,6 +168,143 @@ stick.on("update", (ev) => {
 
 });
 
+init();
+var laserData, selectedPreset, lastButtonPressed;
+var isControllerFxMode=false;
+var isShift=false;
+var isCruiseMode=false;
+var cruiseSpeed=2;
+var cruiseIntervalHandle=-1;
+
+var jsPositions = {
+  "left-stick":{
+    "xInterval":0,
+    "yInterval":0
+  },
+  "right-stick":{
+    "xInterval":0,
+    "yInterval":0
+  },
+  "pad":{
+    "xInterval":0,
+    "yInterval":0
+  },
+  "pad-shift":{
+    "xInterval":0,
+    "yInterval":0
+  }
+}
+
+function init(){
+  laserData={};
+  selectedPreset=1;
+  for(var x=1; x<17; x++){
+      laserData["scene"+x]={"color":1, "gobo":1, "positionX":0, "positionY":0, "scaleX":0, "scaleY":0, "rotation":0, "zoom":0, "animation":0, "dots":0  }
+  }
+  // savePresets();
+  recallPresets();
+}
+
+function recallPresets(){
+  fs.readFile('/root/laserControllerImages/presets1.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    laserData=JSON.parse(data);
+    console.log('recalled saved presets!');
+    emitEvent();
+  });
+  sendArtnet();
+}
+
+function savePresets(){
+  var fileContents=JSON.stringify(laserData);
+  fs.writeFile('/root/laserControllerImages/presets1.txt', fileContents, err => {
+    if (err) {
+      console.error(err);
+    }
+    console.log('saved preset data!');
+  });
+}
+
+function sendArtnet(){
+  var dmxValues=getDMXfromLaserData(laserData["scene"+selectedPreset]);
+  // var dmxValues=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  artnet.set(0,1, dmxValues, function (err, res) {
+    console.log("Sent Artnet");
+    // console.log(dmxValues);
+  });
+}
+
+function getDMXfromLaserData(data){
+  var dmxData=[];
+  var colorMapping=[0,15,18,22,27,32,37,10,50,255];
+  var goboMapping=[3,5,2,29,32,33,75,69,44,43,62,79,85,0,1,7];
+  for(var x=1; x<25; x++){
+      if(x==1 || x==1+12){
+          dmxData.push(255);
+      }else if( x==2 || x==(2+12) ){
+          dmxData.push( colorMapping[data.color-1] );
+      }else if( x==3 || x==(3+12) ){
+          dmxData.push( goboMapping[data.gobo-1] );
+      
+      }else if( x==4 || x==(4+12) ){
+          dmxData.push( data.positionY );
+      }else if( x==5 || x==(5+12) ){
+          dmxData.push( data.positionX );
+      }else if( x==6 || x==(6+12) ){
+          dmxData.push( data.scaleY );
+      }else if( x==7 || x==(7+12) ){
+          dmxData.push( data.scaleX );
+      }else if( x==8 || x==(8+12) ){
+          dmxData.push( data.rotation );
+      }else if( x==9 || x==(9+12) ){
+          dmxData.push( data.zoom );
+      }else if( x==11 || x==(11+12) ){
+          dmxData.push( data.dots );
+      }else if( x==12 || x==(12+12) ){
+          dmxData.push( data.animation );
+      }else{
+          dmxData.push(0);
+      }
+  }
+  return dmxData;
+}
+
+function emitEvent(){
+  var messagePayload={};
+  messagePayload["selectedPreset"]=selectedPreset;
+  messagePayload["laserData"]=laserData;
+  messagePayload["lastButtonPressed"]=lastButtonPressed;
+  messagePayload["isControllerFxMode"]=isControllerFxMode;
+  messagePayload["isCruiseMode"]=isCruiseMode;
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(messagePayload));
+    }
+  });
+  sendArtnet();
+}
+
+function extractJoystickEvents(inputEvent, label, type, number, value, comparator, action){
+  if(comparator==">"){
+    if(inputEvent.type == type && inputEvent.number == number && inputEvent.value > value){
+      console.log(label);
+      lastButtonPressed=label;
+      action();
+      emitEvent();
+    }
+  }else{
+    if(inputEvent.type == type && inputEvent.number == number && inputEvent.value <= value){
+      console.log(label);
+      lastButtonPressed=label;
+      action();
+      emitEvent();
+    }   
+  }
+}
+
 function parseJoystickToPosition(ev, mappedControl, axis){
   var positionRange={"x":45,"y":45};
   var positionCenter={"x":51,"y":51};
@@ -432,7 +437,6 @@ app.get('/toggleCruiseMode', function (req, res) {
   res.send(isCruiseMode);
 });
 
-
 function initCruiseMode(){
   if(isCruiseMode){
     isCruiseMode=false;
@@ -442,9 +446,30 @@ function initCruiseMode(){
     isCruiseMode=true;
     cruiseIntervalHandle=setInterval(function(){
       increaseSelectedPreset();
-    },2000);
+    },(9-cruiseSpeed)*2000);
   }
 }
+
+app.get('/setCruiseSpeed', function (req, res) {
+  setCruiseSpeed(parseInt(req._parsedUrl.query));
+  res.send("done");
+});
+
+function setCruiseSpeed(speed){
+  cruiseSpeed=speed;
+  console.log("set cruiseSpeed: "+cruiseSpeed);
+  if(isCruiseMode){
+    clearInterval(cruiseIntervalHandle);
+    cruiseIntervalHandle=setInterval(function(){
+      increaseSelectedPreset();
+    },(9-cruiseSpeed)*2000);
+  }
+}
+
+app.get('/rebootPi', function (req, res) {
+  res.send("rebooting....");
+  exec('reboot', (err, stdout, stderr) => {});
+});
 
 wss.on('connection', function connection(ws) {
   recallPresets();
