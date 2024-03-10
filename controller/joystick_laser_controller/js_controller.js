b8c614d8397135123c1b62dd7fb0ddb8bece684c
Matt Short <mshort@mobiquityinc.com>
2024-03-10 15:51:44 -0400

    laser_controller - remap js curves

--- controller/joystick_laser_controller/js_controller.js
+++ controller/joystick_laser_controller/js_controller.js
@@ -31,6 +31,7 @@ var cruiseSpeed=2;
 var cruiseIntervalHandle=-1;
 var isGateMode=false;
 var isTriggerOn=false;
+var joystickPolarity=false;
 
 var jsPositions = {
   "left-stick":{
@@ -81,6 +82,10 @@ stick.on("update", (ev) => {
     if(!isControllerFxMode && !isShift){
       isTriggerOn=true;
       console.log("isTriggerOn true");
+    }
+    if(!isControllerFxMode && isShift){
+      joystickPolarity=!joystickPolarity;
+      console.log("Swap JS polarity");
     }
   });
 
@@ -171,10 +176,14 @@ stick.on("update", (ev) => {
 
   extractJoystickEvents(ev, 'left-stick-button', 'BUTTON', 9, 0, ">", function(){
     if(!isControllerFxMode && !isShift){
-      laserData["scene"+selectedPreset]["zoom"]=0;
-      laserData["scene"+selectedPreset]["rotation"]=0;
+      laserData["scene"+selectedPreset]["zoom"]=130;
+      laserData["scene"+selectedPreset]["rotation"]=130;
       laserData["scene"+selectedPreset]["dots"]=0;
-      laserData["scene"+selectedPreset]["animation"]=0;
+      laserData["scene"+selectedPreset]["animation"]=130;
+    }
+    if(!isControllerFxMode && isShift){
+      laserData["scene"+selectedPreset]["positionY"]=130;
+      laserData["scene"+selectedPreset]["scaleY"]=0;
     }
   });
 
@@ -198,8 +207,8 @@ stick.on("update", (ev) => {
 
   extractJoystickEvents(ev, 'right-stick-button', 'BUTTON', 10, 0, ">", function(){
     if(!isControllerFxMode && !isShift){
-      laserData["scene"+selectedPreset]["positionX"]=0;
-      laserData["scene"+selectedPreset]["scaleX"]=0;
+      laserData["scene"+selectedPreset]["positionX"]=130;
+      laserData["scene"+selectedPreset]["scaleX"]=130;
     }
   });
 
@@ -337,21 +346,37 @@ function getDMXfromLaserData(data){
       }else if( x==3 || x==(3+12) ){
           dmxData.push( goboMapping[data.gobo-1] );
       }else if( x==4 || x==(4+12) ){
-          dmxData.push( data.positionY );
+        const positionYMap=[19,19,19,19,20,20,20,20,21,21,21,21,22,22,22,22,23,23,23,23,24,24,24,24,25,25,25,25,26,26,26,26,27,27,27,27,28,28,28,28,29,29,29,29,30,30,30,30,31,31,31,31,32,32,32,32,33,33,33,33,34,34,34,34,35,35,35,35,36,36,36,36,37,37,37,37,38,38,38,38,39,39,39,39,40,40,40,40,41,41,41,41,42,42,42,42,43,43,43,43,44,44,44,44,45,45,45,45,46,46,46,46,47,47,47,47,48,48,48,48,49,49,49,49,50,50,50,50,50,50,50,50,50,50,50,50,51,51,51,51,52,52,52,52,53,53,53,53,54,54,54,54,55,55,55,55,56,56,56,56,57,57,57,57,58,58,58,58,59,59,59,59,60,60,60,60,61,61,61,61,62,62,62,62,63,63,63,63,64,64,64,64,65,65,65,65,66,66,66,66,67,67,67,67,68,68,68,68,69,69,69,69,70,70,70,70,71,71,71,71,72,72,72,72,73,73,73,73,74,74,74,74,75,75,75,75,76,76,76,76,77,77,77,77,78,78,78,78,79,79,79,79,80,80,80,80];
+        dmxData.push( positionYMap[data.positionY] );
       }else if( x==5 || x==(5+12) ){
-          dmxData.push( data.positionX );
+        const positionXMap=[182,174,170,166,164,162,160,159,158,158,158,158,157,157,157,157,156,156,156,156,155,155,155,155,154,154,154,154,153,153,153,153,152,152,152,152,151,151,151,151,150,150,150,150,149,149,149,149,148,148,148,148,147,147,147,147,146,146,146,146,145,145,145,145,144,144,144,144,143,143,143,143,142,142,142,142,141,141,141,141,140,140,140,140,139,139,139,139,138,138,138,138,137,137,137,137,136,136,136,136,135,135,135,135,134,134,134,134,133,133,133,133,132,132,132,132,131,131,131,131,130,130,130,130,0,0,0,0,0,0,0,0,0,0,0,0,192,192,192,192,193,193,193,193,194,194,194,194,195,195,195,195,196,196,196,196,197,197,197,197,198,198,198,198,199,199,199,199,200,200,200,200,201,201,201,201,202,202,202,202,203,203,203,203,204,204,204,204,205,205,205,205,206,206,206,206,207,207,207,207,208,208,208,208,209,209,209,209,210,210,210,210,211,211,211,211,212,212,212,212,213,213,213,213,214,214,214,214,215,215,215,215,216,216,216,216,217,217,217,217,218,218,218,218,219,219,219,219,220,222,224,228,234,238,242,246];
+        if(joystickPolarity){
+          dmxData.push( positionXMap[255-data.positionX] );
+        }else{
+          dmxData.push( positionXMap[data.positionX] );
+        }
       }else if( x==6 || x==(6+12) ){
-          dmxData.push( data.scaleY );
+        const scaleYMap=[0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14,15,15,15,15,16,16,16,16,17,17,17,17,18,18,18,18,19,19,19,19,20,20,20,20,21,21,21,21,22,22,22,22,23,23,23,23,24,24,24,24,25,25,25,25,26,26,26,26,27,27,27,27,28,28,28,28,29,29,29,29,30,30,30,30,31,31,31,31,32,32,32,32,33,33,33,33,34,34,34,34,35,35,35,35,36,36,36,36,37,37,37,37,38,38,38,38,39,39,39,39,40,40,40,40,41,41,41,41,42,42,42,42,43,43,43,43,44,44,44,44,45,45,45,45,46,46,46,46,47,47,47,47,48,48,48,48,49,49,49,49,50,50,50,50,51,51,51,51,52,52,52,52,53,53,53,53,54,54,54,54,55,55,55,55,56,56,56,56,57,57,57,57,58,58,58,58,59,59,59,59,60,60,60,60,61,61,61,61,62,62,62,62,63,63,63,63];
+        dmxData.push( scaleYMap[data.scaleY] );
       }else if( x==7 || x==(7+12) ){
-          dmxData.push( data.scaleX );
+        const scaleXMap=[176,168,164,160,159,159,159,159,158,158,158,158,157,157,157,157,156,156,156,156,155,155,155,155,154,154,154,154,153,153,153,153,152,152,152,152,151,151,151,151,150,150,150,150,149,149,149,149,148,148,148,148,147,147,147,147,146,146,146,146,145,145,145,145,144,144,144,144,143,143,143,143,142,142,142,142,141,141,141,141,140,140,140,140,139,139,139,139,138,138,138,138,137,137,137,137,136,136,136,136,135,135,135,135,134,134,134,134,133,133,133,133,132,132,132,132,131,131,131,131,130,130,130,130,129,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,129,129,129,129,130,130,130,130,131,131,131,131,132,132,132,132,133,133,133,133,134,134,134,134,135,135,135,135,136,136,136,136,137,137,137,137,138,138,138,138,139,139,139,139,140,140,140,140,141,141,141,141,142,142,142,142,143,143,143,143,144,144,144,144,145,145,145,145,146,146,146,146,147,147,147,147,148,148,148,148,149,149,149,149,150,150,150,150,151,151,151,151,152,152,152,152,153,153,153,153,154,154,154,154,155,155,155,155,156,156,156,156,157,164,168,176];
+        dmxData.push( scaleXMap[data.scaleX]);
       }else if( x==8 || x==(8+12) ){
-          dmxData.push( data.rotation );
+        const rotationMap=[238,230,228,226,224,222,220,220,219,219,219,219,218,218,218,218,217,217,217,217,216,216,216,216,215,215,215,215,214,214,214,214,213,213,213,213,212,212,212,212,211,211,211,211,210,210,210,210,209,209,209,209,208,208,208,208,207,207,207,207,206,206,206,206,205,205,205,205,204,204,204,204,203,203,203,203,202,202,202,202,201,201,201,201,200,200,200,200,199,199,199,199,198,198,198,198,197,197,197,197,196,196,196,196,195,195,195,195,194,194,194,194,193,193,193,193,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,128,128,128,128,129,129,129,129,130,130,130,130,131,131,131,131,132,132,132,132,133,133,133,133,134,134,134,134,135,135,135,135,136,136,136,136,137,137,137,137,138,138,138,138,139,139,139,139,140,140,140,140,141,141,141,141,142,142,142,142,143,143,143,143,144,144,144,144,145,145,145,145,146,146,146,146,147,147,147,147,148,148,148,148,149,149,149,149,150,150,150,150,151,151,151,151,152,152,152,152,153,153,153,153,154,154,154,154,155,155,155,155,156,158,160,162,164,166,168,170,172,176,182];  
+        if(joystickPolarity){
+          dmxData.push( rotationMap[data.rotation] );
+        }else{
+          dmxData.push( rotationMap[255-data.rotation] );
+        }
+        
       }else if( x==9 || x==(9+12) ){
-          dmxData.push( data.zoom );
+        const zoomMap=[118,118,118,118,117,117,117,117,116,116,116,116,115,115,115,115,114,114,114,114,113,113,113,113,112,112,112,112,111,111,111,111,110,110,110,110,109,109,109,109,108,108,108,108,107,107,107,107,106,106,106,106,105,105,105,105,104,104,104,104,103,103,103,103,102,102,102,102,101,101,101,101,100,100,100,100,99,99,99,99,98,98,98,98,97,97,97,97,96,96,96,96,95,95,95,95,94,94,94,94,93,93,93,93,92,92,92,92,91,91,91,91,90,90,90,90,89,89,89,89,88,88,88,88,87,87,87,87,0,0,0,0,0,0,0,0,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14,15,15,15,15,16,16,16,16,17,17,17,17,18,18,18,18,19,19,19,19,20,20,20,20,21,21,21,21,22,22,22,22,23,23,23,23,24,24,24,24,25,25,25,25,26,26,26,26,27,27,27,27,28,28,28,28,29,29,29,29,30,30,30,30,31,31,31,31,32,32,32,32,33,33,33,33,34,34,34,34,35,35,35,35,36,36,36,36,37,37,37,37];
+        dmxData.push( zoomMap[data.zoom] );
       }else if( x==11 || x==(11+12) ){
           dmxData.push( data.dots );
       }else if( x==12 || x==(12+12) ){
-          dmxData.push( data.animation );
+        const animationMap=[118,118,118,118,117,117,117,117,116,116,116,116,115,115,115,115,114,114,114,114,113,113,113,113,112,112,112,112,111,111,111,111,110,110,110,110,109,109,109,109,108,108,108,108,107,107,107,107,106,106,106,106,105,105,105,105,104,104,104,104,103,103,103,103,102,102,102,102,101,101,101,101,100,100,100,100,99,99,99,99,98,98,98,98,97,97,97,97,96,96,96,96,95,95,95,95,94,94,94,94,93,93,93,93,92,92,92,92,91,91,91,91,90,90,90,90,89,89,89,89,88,88,88,88,87,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14,15,15,15,15,16,16,16,16,17,17,17,17,18,18,18,18,19,19,19,19,20,20,20,20,21,21,21,21,22,22,22,22,23,23,23,23,24,24,24,24,25,25,25,25,26,26,26,26,27,27,27,27,28,28,28,28,29,29,29,29,30,30,30,30,31,31,31,31,32,32,32,32,33,33,33,33,34,34,34,34,35,35,35,35,36,36,36,36,37,37,37,37];
+        dmxData.push( animationMap[data.animation] );
       }else{
           dmxData.push(0);
       }
@@ -405,20 +430,25 @@ function parseJoystickToSlider(ev, stick, axisInterval, mappedControl, incAdjust
   var maxLimit=255;
   var minLimit=0;
   if(mappedControl=="positionY"){
-    var maxSliderInc=1;
-    var maxLimit=81;
-    var minLimit=19;
+    var maxSliderInc=3;
+    // var maxLimit=81;
+    // var minLimit=19;
   }
   if(mappedControl=="scaleY"){
-    var maxSliderInc=1;
-    var maxLimit=64;
-    var minLimit=0;
+    var maxSliderInc=3;
+    // var maxLimit=64;
+    // var minLimit=0;
   }
   if(mappedControl=="animation"){
-    var maxSliderInc=4;
+    var maxSliderInc=5;
   }
   if(mappedControl=="dots"){
-    var maxSliderInc=15;
+    var maxSliderInc=30;
+  }
+  if(mappedControl=="rotation"){
+    var maxSliderInc=3;
+    var maxLimit=255;
+    var minLimit=0;
   }
   if(mappedControl=="positionX" ||mappedControl=="rotation" || mappedControl=="dots" || mappedControl=="positionY"){
     ev.value=ev.value*-1;
@@ -431,15 +461,22 @@ function parseJoystickToSlider(ev, stick, axisInterval, mappedControl, incAdjust
       var sliderInc=Math.abs(Math.floor(ev.value/32767*maxSliderInc));
       if(Math.sign(ev.value)==-1){
         if( laserData["scene"+selectedPreset][mappedControl]<(maxLimit-sliderInc) ){
-          laserData["scene"+selectedPreset][mappedControl]=laserData["scene"+selectedPreset][mappedControl]+(sliderInc+incAdjustment);    
+          laserData["scene"+selectedPreset][mappedControl]=laserData["scene"+selectedPreset][mappedControl]+(sliderInc+incAdjustment);
         }else{
           laserData["scene"+selectedPreset][mappedControl]=maxLimit;
         }
+        // if(mappedControl=="rotation" && laserData["scene"+selectedPreset][mappedControl]<=(minLimit)){
+        //   laserData["scene"+selectedPreset][mappedControl]=minLimit;
+        // }
       }else if(Math.sign(ev.value)==1){
         if( laserData["scene"+selectedPreset][mappedControl]>(minLimit+sliderInc) ){
           laserData["scene"+selectedPreset][mappedControl]=laserData["scene"+selectedPreset][mappedControl]-(sliderInc+incAdjustment);    
         }else{
-          laserData["scene"+selectedPreset][mappedControl]=minLimit;
+          // if(mappedControl=="rotation"){
+          //   laserData["scene"+selectedPreset][mappedControl]=0;
+          // }else{
+            laserData["scene"+selectedPreset][mappedControl]=minLimit;
+          // }
         }
       }
       if(mappedControl=="scaleY" || mappedControl=="positionY"){
