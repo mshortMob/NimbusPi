bfaf5fdf023024b6d4a53d0e24f6d5377ad50363
Matthew Short <matt.short@brightmls.com>
2025-02-19 18:22:02 -0500

    Initial Port to platformIO - only a few dep issues remaining

---
+++ esp8266_Firmware/Arnet_Pixel_Controler_V3/src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <button_funcs.h>
+#include <led_funcs.h>
+#include <eeprom_funcs.h>
+#include <socket_server.h>
+#include <http_server.h>
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+  setup_leds();
+  setup_eeprom();
+  setup_ap(epdata.ssid, epdata.password);
+  setup_http_server();
+  // setup_socket_server();
+  setup_button();
+}
+
+void loop() {
+  int currentMode=handle_button();
+  handle_leds(currentMode);
+  // handle_socket_server();
+  handle_http_server();
+}
--- esp8266_Firmware/Arnet_Pixel_Controler_V3/src/socket_server.h
+++ esp8266_Firmware/Arnet_Pixel_Controler_V3/src/socket_server.h
@@ -1,58 +1,57 @@
 #include "ap_funcs.h" 
-#include <WebSocketsServer_Generic.h>
-#include <Arduino.h>
+// #include <WebSocketsServer_Generic.h>
 #include <Hash.h>
 #include <ArduinoJson.h>
 
 
-String old_value, value;
-int lastSocketSendTime=0;
+// String old_value, value;
+// int lastSocketSendTime=0;
 
-WebSocketsServer    webSocket = WebSocketsServer(81);
+// WebSocketsServer    webSocket = WebSocketsServer(81);
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-  switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.printf("[%u] Disconnected!\n", num);
-      break;
+// void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+//   switch (type) {
+//     case WStype_DISCONNECTED:
+//       Serial.printf("[%u] Disconnected!\n", num);
+//       break;
 
-    case WStype_CONNECTED: {
-        IPAddress ip = webSocket.remoteIP(num);
-        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-        // send message to client
-        webSocket.sendTXT(num, "0");
-      }
-      break;
+//     case WStype_CONNECTED: {
+//         IPAddress ip = webSocket.remoteIP(num);
+//         Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+//         // send message to client
+//         webSocket.sendTXT(num, "0");
+//       }
+//       break;
 
-    case WStype_TEXT:
-      Serial.printf("[%u] get Text: %s\n", num, payload);
-      cycle_mode();
-      // send message to client
-      // webSocket.sendTXT(num, "message here");
-      // send data to all connected clients
-      // webSocket.broadcastTXT("message here");
-      break;
+//     case WStype_TEXT:
+//       Serial.printf("[%u] get Text: %s\n", num, payload);
+//       cycle_mode();
+//       // send message to client
+//       // webSocket.sendTXT(num, "message here");
+//       // send data to all connected clients
+//       // webSocket.broadcastTXT("message here");
+//       break;
       
-    case WStype_BIN:
-      Serial.printf("[%u] get binary length: %u\n", num, length);
-      hexdump(payload, length);
-      // send message to client
-      // webSocket.sendBIN(num, payload, length);
-      break;
-  }
+//     case WStype_BIN:
+//       Serial.printf("[%u] get binary length: %u\n", num, length);
+//       hexdump(payload, length);
+//       // send message to client
+//       // webSocket.sendBIN(num, payload, length);
+//       break;
+//   }
 
-}
+// }
 
-void setup_socket_server(){
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
-}
+// void setup_socket_server(){
+//   webSocket.begin();
+//   webSocket.onEvent(webSocketEvent);
+// }
 
-void handle_socket_server(){
-  webSocket.loop();
-  if( (millis()-lastSocketSendTime) > 500 ){
-    value = (String) analogRead(0);
-    webSocket.broadcastTXT(value); 
-    lastSocketSendTime=millis();
-  }
-}
+// void handle_socket_server(){
+//   webSocket.loop();
+//   if( (millis()-lastSocketSendTime) > 500 ){
+//     value = (String) analogRead(0);
+//     webSocket.broadcastTXT(value); 
+//     lastSocketSendTime=millis();
+//   }
+// }
