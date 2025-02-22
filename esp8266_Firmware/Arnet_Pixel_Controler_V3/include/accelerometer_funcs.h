654aa2e446b2cdda60b9a6ba8748a26bb4479f1d
Matthew Short <matt.short@brightmls.com>
2025-02-22 12:42:20 -0500

    Initial Port to platformIO - add accellerometer funcs

---
+++ esp8266_Firmware/Arnet_Pixel_Controler_V3/include/accelerometer_funcs.h
@@ -0,0 +1,92 @@
+#include<Wire.h>
+
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+float valx;
+float valy;
+float valz;
+float lastx=0.0;
+float lasty=0.0;
+float lastz=0.0;
+float diffx=0.0;
+float diffy=0.0;
+float diffz=0.0;
+
+float getAccelData(char* chan){
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true);  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read(); 
+  Tmp=Wire.read()<<8|Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+
+  if(chan=="x"){
+    return AcX;
+  }
+  if(chan=="y"){
+    return AcY;
+  }
+  if(chan=="z"){
+    return AcZ;
+  }else{
+    return AcZ;
+  }
+}
+
+float applyMaxAndMinLimits(float in){
+  float out=in;
+  if(out<0.0){
+    out=0.0;
+  }
+  if(out>1){
+    out=1.0;
+  }
+  return out;
+}
+
+void scrapeAccellerometer(bool logging){
+  valx=applyMaxAndMinLimits((getAccelData("x")+16000)/100000*3.0);
+  valy=applyMaxAndMinLimits((getAccelData("y")+16000)/100000*3.0);
+  valz=applyMaxAndMinLimits((getAccelData("z")+16000)/100000*3.0);
+  
+  diffx=lastx-valx;
+  diffy=lasty-valy;
+  diffz=lastz-valz;
+  if(diffx<0){
+    diffx=diffx*-1.0;
+  }
+  if(diffy<0){
+    diffy=diffy*-1.0;
+  }
+  if(diffz<0){
+    diffz=diffz*-1.0;
+  }
+  lastx=valx;
+  lasty=valy;
+  lastz=valz;
+
+  if(logging==true){
+    Serial.print(valx);
+    Serial.print("  ");
+    Serial.print(valy);
+    Serial.print("  ");
+    Serial.println(valz);
+  }
+}
+
+void setup_accelerometer() {
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+}
+
+void handle_accelerometer() {
+  scrapeAccellerometer(false);
+}
--- esp8266_Firmware/Arnet_Pixel_Controler_V3/src/main.cpp
+++ esp8266_Firmware/Arnet_Pixel_Controler_V3/src/main.cpp
@@ -4,6 +4,7 @@
 #include <eeprom_funcs.h>
 #include <socket_server.h>
 #include <http_server.h>
+#include <accelerometer_funcs.h>
 
 void setup() {
   delay(1000);
@@ -15,10 +16,12 @@ void setup() {
   setup_http_server();
   setup_socket_server();
   setup_button();
+  setup_accelerometer();
 }
 
 void loop() {
   int currentMode=handle_button();
   handle_leds(currentMode);
   handle_socket_server();
+  handle_accelerometer();
 }
